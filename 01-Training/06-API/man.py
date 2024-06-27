8e8fecf692a36bd8b6bc8fb58f0ec56f9b6a0535
AdrianoR85 <adrianorosa085@gmail.com>
2024-06-27 16:23:05 -0300

    ecommerce project 01

--- 01-Training/06-API/man.py
+++ 01-Training/06-API/man.py
@@ -15,7 +15,7 @@ if response.status_code == 200:
   response_data = response.json()
   df = pd.json_normalize(response_data, 'data')
   df = df[['id','symbol','name', 'priceUsd' ]]
-  engine = create_engine('mysql+pymysql://root:root@localhost:3306/python_test')
-  df.to_sql('crypto_data', engine, if_exists='replace', index=False)
+  connction = create_engine('mysql+pymysql://<USER>:<PASSWORD>@<HOST>:<PORT>/<DATABASE_NAME>')
+  df.to_sql('crypto_data', connction, if_exists='replace', index=False)
 else:
   print('Failed to get data')
