0d1bb1f58d62684e61e3e85e9c77630c73ced174
adrianor85 <adrianorosa085@gmail.com>
2024-07-18 13:38:12 -0300

    python back end

---
+++ 06-Backend/01_good_habits.py
@@ -0,0 +1,7 @@
+import os
+
+fruits = ["apple", "orange", "banana", "orange"]
+print(
+    """dsfdfdsf sdfsdfsaf sadfsdfsd asfasdfsdf asdfsdfsa sdfsdfsdf asdfsadfsaf asdfsdf asdfsdfs sadfsdfsdf asdfsdfsdf 
+    sdafsadfsaf sdfasf dsadfasdf asdfsdfasdfsadf asdfasdfdsfsaf asdfasdf asdfasdfasfa sfsf"""
+)
