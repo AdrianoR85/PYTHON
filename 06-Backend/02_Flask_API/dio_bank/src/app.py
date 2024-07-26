7bfe25f084eba3b026ae791017659f026ac67c28
adrianor85 <adrianorosa085@gmail.com>
2024-07-26 15:38:54 -0300

    python - flask

--- 06-Backend/02_Flask_API/dio_bank/src/app.py
+++ 06-Backend/02_Flask_API/dio_bank/src/app.py
@@ -1,21 +1,12 @@
 from flask import Flask
-<<<<<<< HEAD
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Hello, World!</h1>"
 
-
-=======
-app = Flask(__name__)
-
-@app.route("/<user>")
-def Home(user):
-    return f"<h1>Welcome, {user}</h1>"
-
-@app.route("/about")
+@app.route("/about", methods=["GET", "POST"])
 def about():
     return "<h1>About</h1><p>This is a simple Flask application.</p>"
->>>>>>> flask_router
+
