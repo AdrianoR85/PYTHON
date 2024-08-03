6af0ddb07359ba227b878ac56ad1d8049017db89
adrianor85 <adrianorosa085@gmail.com>
2024-08-03 14:05:33 -0300

    python

--- 06-Backend/02_Flask_API/dio_bank/src/app.py
+++ 06-Backend/02_Flask_API/dio_bank/src/app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import click
 import os
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+@click.command('init-db')
+def init_db_command():
+    global db
+    click.echo('Initialized the database.')
+    with current_app.app_context():
+        db.create_all()
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'diobank.sqlite'),
+        SQLALCHEMY_DATABASE_URI= 'data/diobank.sqlite',
+
     )
 
     if test_config is None:
@@ -18,9 +35,15 @@ def create_app(test_config=None):
 
     # ensure the instance folder exists
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs('data')
     except OSError:
         pass
+    
+    # register the database commands
+    app.cli.add_command(init_db_command)
 
+    # initialize Extensions
+    db.init_app(app)
+    
     return app
 
