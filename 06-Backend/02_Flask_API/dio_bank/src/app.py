389616cb0b7746995f1e070074281f062b243355
AdrianoR85 <adrianorosa085@gmail.com>
2024-08-10 14:29:11 -0300

    Python

--- 06-Backend/02_Flask_API/dio_bank/src/app.py
+++ 06-Backend/02_Flask_API/dio_bank/src/app.py
@@ -1,49 +1,61 @@
 from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+import sqlalchemy as sa
 import click
 import os
 
+
 class Base(DeclarativeBase):
-  pass
+    pass
+
 
 db = SQLAlchemy(model_class=Base)
 
-@click.command('init-db')
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.String, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r}"
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.String, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+    body: Mapped[str] = mapped_column(sa.Text, nullable=False)
+    create_app: Mapped[str] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[str] = mapped_column(sa.String, sa.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r}"
+
+@click.command("init-db")
 def init_db_command():
     global db
-    click.echo('Initialized the database.')
+    """Clear the existing data and create new tables."""
     with current_app.app_context():
         db.create_all()
+    click.echo("Initialized the database.")
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI= 'data/diobank.sqlite',
-
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI="sqlite:///diobank.sqlite",
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs('data')
-    except OSError:
-        pass
-    
-    # register the database commands
     app.cli.add_command(init_db_command)
-
-    # initialize Extensions
     db.init_app(app)
-    
-    return app
 
+    return app
